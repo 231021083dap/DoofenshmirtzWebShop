3c4c4d182c63c5adc6b143e285281bc1104511e1
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2021-09-13 13:05:59 +0200

    Added the extra things on Products

--- DoofenshmirtzsWebShop/Database/Entities/Products.cs
+++ DoofenshmirtzsWebShop/Database/Entities/Products.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace DoofenshmirtzWebShopTests
+namespace DoofenshmirtzsWebShop.Database.Entities
 {
-    class ProductsServiceTests
+    public class Products
     {
 
     }
