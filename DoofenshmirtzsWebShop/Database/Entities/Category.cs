3ff719ea81fec98d58a55dd4a61f9294d685e2cd
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2021-09-14 10:55:29 +0200

    Added reference between the API project and the first test project as well as added all the entityframework nuget packets

--- DoofenshmirtzsWebShop/Database/Entities/Category.cs
+++ DoofenshmirtzsWebShop/Database/Entities/Category.cs
@@ -7,6 +7,6 @@ namespace DoofenshmirtzsWebShop.Database.Entities
 {
     public class Category
     {
-        
+           
     }
 }
--- DoofenshmirtzsWebshopCategoryTests/CategoryControllerTests.cs
+++ DoofenshmirtzsWebshopCategoryTests/CategoryControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoofenshmirtzsWebShop.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace DoofenshmirtzsWebshopCategoryTests
 {
     class CategoryControllerTests
     {
+        
     }
 }
