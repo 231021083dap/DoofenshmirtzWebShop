5e28c57cba69638c751dce4a01c3c93d2c606ab1
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2021-09-14 17:40:40 +0200

    Changed back to original push from the Branch - Data

--- DoofenshmirtzsWebShop/Repositories/CategoryRepository.cs
+++ DoofenshmirtzsWebShop/Repositories/CategoryRepository.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using DoofenshmirtzsWebShop.Database;
+using DoofenshmirtzsWebShop.Database.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DoofenshmirtzsWebShop.Repositories
 {
+    
     public class CategoryRepository
     {
+       
     }
 }
--- DoofenshmirtzsWebShop/Services/CategoryServices.cs
+++ DoofenshmirtzsWebShop/Services/CategoryServices.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using DoofenshmirtzsWebShop.Database.Entities;
+using DoofenshmirtzsWebShop.DTOs.Responses;
+using DoofenshmirtzsWebShop.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DoofenshmirtzsWebShop.Services
 {
+
     public class CategoryServices
     {
+       
     }
 }
