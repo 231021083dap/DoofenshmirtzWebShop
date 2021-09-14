971dd0889bceab599524d925714b21afe154d7b8
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2021-09-14 12:36:05 +0200

    Added the 3 extra controllers with additional DTO's, entities, requests, responses, repositories, services and added test environment on all the new controllers

---
+++ DoofenshmirtzsWebShop/DTOs/Requests/UpdateProduct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoofenshmirtzsWebShop.DTOs.Requests
+{
+    public class UpdateProduct
+    {
+    }
+}
