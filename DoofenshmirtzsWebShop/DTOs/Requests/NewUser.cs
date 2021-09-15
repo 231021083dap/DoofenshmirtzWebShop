164fe687423213dceb2535870354042aad4454a0
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2021-09-15 13:57:17 +0200

    Added the last missing requests

--- DoofenshmirtzsWebShop/DTOs/Requests/NewUser.cs
+++ DoofenshmirtzsWebShop/DTOs/Requests/NewUser.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace DoofenshmirtzsWebShop.DTOs.Requests
 {
     public class NewUser
     {
+        [Required]
+        [Column(TypeName = "nvarchar(320)")]
+        public string userEmail { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string userPassword { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string userName { get; set; }
+
+        public Helpers.Role userRole { get; set; }
     }
 }
--- DoofenshmirtzsWebShop/DTOs/Requests/UpdateProduct.cs
+++ DoofenshmirtzsWebShop/DTOs/Requests/UpdateProduct.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,23 @@ namespace DoofenshmirtzsWebShop.DTOs.Requests
 {
     public class UpdateProduct
     {
+        [Required]
+        [StringLength(100, ErrorMessage = "Productname has to be less than 100 chars")]
+        [MinLength(1, ErrorMessage = "Productname must contain at least 1 char")]
+        public string productName { get; set; }
+
+        [Required]
+        public int productPrice { get; set; }
+
+        [Required]
+        public int productStock { get; set; }
+
+        [Required]
+        [StringLength(3200, ErrorMessage = "Too long bruh - just too long!")]
+        [MinLength(1, ErrorMessage = "At least add a dot")]
+        public string productDescription { get; set; }
+
+        [Required]
+        public int categoryID { get; set; }
     }
 }
