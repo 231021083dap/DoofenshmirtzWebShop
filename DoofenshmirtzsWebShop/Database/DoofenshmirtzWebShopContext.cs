b4da94f06f92fe69b7933388a4f39bd8b00da2f1
NepNep <58166534+NepBullV2@users.noreply.github.com>
2021-09-16 12:44:36 +0200

    Finished entire Product Section

--- DoofenshmirtzsWebShop/Database/DoofenshmirtzWebShopContext.cs
+++ DoofenshmirtzsWebShop/Database/DoofenshmirtzWebShopContext.cs
@@ -14,6 +14,7 @@ namespace DoofenshmirtzsWebShop.Database
         public DoofenshmirtzWebShopContext(DbContextOptions<DoofenshmirtzWebShopContext> options) : base(options) { }
 
         public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -34,6 +35,33 @@ namespace DoofenshmirtzsWebShop.Database
                     categoryName = "Merch"
                 }
             );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    productID = 1,
+                    productName = "The I-Don't-Care-Inator",
+                    productStock = 7,
+                    productPrice = 49,
+                    categoryID = 1
+                },
+                new Product
+                {
+                    productID = 2,
+                    productName = "Kill-half-the-people-in-the-world-with-a-snap-Inator",
+                    productStock = 2,
+                    productPrice = 299,
+                    categoryID = 1
+                },
+                new Product
+                {
+                    productID = 3,
+                    productName = "Shut-The-Hell-Up-Inator",
+                    productStock = 8,
+                    productPrice = 50,
+                    categoryID = 1
+                }
+                );
         }
 
     }
--- DoofenshmirtzsWebShop/Repositories/ProductRepository.cs
+++ DoofenshmirtzsWebShop/Repositories/ProductRepository.cs
@@ -1,17 +1,23 @@
 ﻿using DoofenshmirtzsWebShop.Database;
 using DoofenshmirtzsWebShop.Database.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace DoofenshmirtzsWebShop.Repositories
 {
     public interface IProductRepository
     {
-        Task<Product> getAllProducts();
+        Task<List<Product>> getAllProducts();
+        Task<Product> getProductById(int productId);
+        Task<Product> newProduct(Product product);
+        Task<Product> updateProduct(int productiId, Product product);
+        Task<Product> deleteProduct(int productId);
     }
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
         private readonly DoofenshmirtzWebShopContext _productContext;
         public ProductRepository(DoofenshmirtzWebShopContext context)
@@ -21,7 +27,40 @@ namespace DoofenshmirtzsWebShop.Repositories
 
         public async Task<List<Product>> getAllProducts()
         {
-            return await _productContext
+            return await _productContext.Product.Include(a => a.categoryID).ToListAsync();
+        }
+        public async Task<Product> getProductById(int productId)
+        {
+            return await _productContext.Product.Include(a => a.categoryID).FirstOrDefaultAsync(a => a.productID == productId);
+        }
+        public async Task<Product> newProduct(Product product)
+        {
+            _productContext.Product.Add(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+        public async Task<Product> updateProduct(int productId, Product product)
+        {
+            Product updateProduct = await _productContext.Product.FirstOrDefaultAsync(a => a.productID == productId);
+            if (product != null)
+            {
+                updateProduct.productName = product.productName;
+                updateProduct.productDescription = product.productDescription;
+                updateProduct.productStock = product.productStock;
+                updateProduct.productPrice = product.productPrice;
+                updateProduct.categoryID = product.categoryID;
+            }
+            return updateProduct;
+        }
+        public async Task<Product> deleteProduct(int productId)
+        {
+            Product product = await _productContext.Product.FirstOrDefaultAsync(a => a.productID == productId);
+            if (product != null)
+            {
+                _productContext.Product.Remove(product);
+                await _productContext.SaveChangesAsync();
+            }
+            return product;
         }
     }
 }
