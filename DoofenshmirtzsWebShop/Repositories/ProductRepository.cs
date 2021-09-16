5b97d19642fac65012c187c12ab987b86c6ff0ff
NepNep <58166534+NepBullV2@users.noreply.github.com>
2021-09-16 09:48:35 +0200

    Added Product Branch

--- DoofenshmirtzsWebShop/Repositories/ProductRepository.cs
+++ DoofenshmirtzsWebShop/Repositories/ProductRepository.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using DoofenshmirtzsWebShop.Database;
+using DoofenshmirtzsWebShop.Database.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DoofenshmirtzsWebShop.Repositories
 {
+    public interface IProductRepository
+    {
+        Task<Product> getAllProducts();
+    }
     public class ProductRepository
     {
+        private readonly DoofenshmirtzWebShopContext _productContext;
+        public ProductRepository(DoofenshmirtzWebShopContext context)
+        {
+            _productContext = context;
+        }
+
+        public async Task<List<Product>> getAllProducts()
+        {
+            return await _productContext
+        }
     }
 }
