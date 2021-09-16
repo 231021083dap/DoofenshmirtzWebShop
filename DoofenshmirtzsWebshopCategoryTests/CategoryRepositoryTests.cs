605998fa1d172d2ba6319827aa371f87bd3169bd
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2021-09-16 13:20:20 +0200

    Category added to completion - adding tests now

--- DoofenshmirtzsWebshopCategoryTests/CategoryRepositoryTests.cs
+++ DoofenshmirtzsWebshopCategoryTests/CategoryRepositoryTests.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using DoofenshmirtzsWebShop.Database;
+using DoofenshmirtzsWebShop.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace DoofenshmirtzsWebshopCategoryTests
 {
-    class CategoryRepositoryTests
+    public class CategoryRepositoryTests
     {
+        private readonly CategoryRepository _sut;
+        private readonly DoofenshmirtzWebShopContext _context;
+        private readonly DbContextOptions<DoofenshmirtzWebShopContext> _options;
+
+        public CategoryRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<DoofenshmirtzWebShopContext>()
+                .UseInMemoryDatabase(databaseName: "DoofenshmirtzWebShopCatagory")
+                .Options;
+
+            _sut = new CategoryRepository(_context);
+        }
+
+        [Fact]
+        public async Task getAll_shouldReturnListOfCategories_whenCategoriesExists()
+        {
+            
+        }
     }
 }
