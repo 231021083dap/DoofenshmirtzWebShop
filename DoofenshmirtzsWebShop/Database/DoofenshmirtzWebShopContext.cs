bf88364ed312a447727745df42e295692d7fc1e0
Jeppe Marcussen <jeppem65@gmail.com>
2021-09-16 13:51:33 +0200

    Changes to DbContext

--- DoofenshmirtzsWebShop/Database/DoofenshmirtzWebShopContext.cs
+++ DoofenshmirtzsWebShop/Database/DoofenshmirtzWebShopContext.cs
@@ -15,6 +15,7 @@ namespace DoofenshmirtzsWebShop.Database
 
         public DbSet<Category> Category { get; set; }
         public DbSet<Order> Order { get; set; }
+        public DbSet<Product> Product { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -56,6 +57,31 @@ namespace DoofenshmirtzsWebShop.Database
                  }
 
                 );
+            modelBuilder.Entity<Product>().HasData(
+    new Product
+    {
+        productID = 1,
+        productName = "The I-Don't-Care-Inator",
+        productStock = 7,
+        productPrice = 49,
+        categoryID = 1
+    },
+    new Product
+    {
+        productID = 2,
+        productName = "Kill-half-the-people-in-the-world-with-a-snap-Inator",
+        productStock = 2,
+        productPrice = 299,
+        categoryID = 1
+    },
+    new Product
+    {
+        productID = 3,
+        productName = "Shut-The-Hell-Up-Inator",
+        productStock = 8,
+        productPrice = 50,
+        categoryID = 1
+    });
         }
 
     }
