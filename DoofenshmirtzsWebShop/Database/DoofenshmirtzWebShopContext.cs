04e5bb60bbe0d8b5a8a1a1b35caaa8c6732c3b77
Jeppe Marcussen <jeppem65@gmail.com>
2021-09-16 13:53:38 +0200

    Changes to Db Context

--- DoofenshmirtzsWebShop/Database/DoofenshmirtzWebShopContext.cs
+++ DoofenshmirtzsWebShop/Database/DoofenshmirtzWebShopContext.cs
@@ -57,31 +57,7 @@ namespace DoofenshmirtzsWebShop.Database
                  }
 
                 );
-            modelBuilder.Entity<Product>().HasData(
-    new Product
-    {
-        productID = 1,
-        productName = "The I-Don't-Care-Inator",
-        productStock = 7,
-        productPrice = 49,
-        categoryID = 1
-    },
-    new Product
-    {
-        productID = 2,
-        productName = "Kill-half-the-people-in-the-world-with-a-snap-Inator",
-        productStock = 2,
-        productPrice = 299,
-        categoryID = 1
-    },
-    new Product
-    {
-        productID = 3,
-        productName = "Shut-The-Hell-Up-Inator",
-        productStock = 8,
-        productPrice = 50,
-        categoryID = 1
-    });
+           
         }
 
     }
