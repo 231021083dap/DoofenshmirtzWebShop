eeec9d8555e328cac47fde75e088f36e6c0a2974
NepNep <58166534+NepBullV2@users.noreply.github.com>
2021-09-17 13:25:20 +0200

    Added all components

---
+++ AngularClient/AngularClient/src/app/admin/admin-new-category/admin-new-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-new-category',
+  templateUrl: './admin-new-category.component.html',
+  styleUrls: ['./admin-new-category.component.css']
+})
+export class AdminNewCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- AngularClient/AngularClient/src/app/app.module.ts
+++ AngularClient/AngularClient/src/app/app.module.ts
@@ -3,10 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { CartComponent } from './cart/cart.component';
+import { SingleProductComponent } from './single-product/single-product.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { AdminDatabaseComponent } from './admin/admin-database/admin-database.component';
+import { AdminNewCategoryComponent } from './admin/admin-new-category/admin-new-category.component';
+import { AdminNewProductComponent } from './admin/admin-new-product/admin-new-product.component';
+import { AdminNewuserComponent } from './admin/admin-newuser/admin-newuser.component';
+import { AdminNewRoleComponent } from './admin/admin-new-role/admin-new-role.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    ProductsComponent,
+    LoginComponent,
+    SignupComponent,
+    CartComponent,
+    SingleProductComponent,
+    ContactComponent,
+    AboutComponent,
+    UserPageComponent,
+    AdminDashboardComponent,
+    AdminDatabaseComponent,
+    AdminNewCategoryComponent,
+    AdminNewProductComponent,
+    AdminNewuserComponent,
+    AdminNewRoleComponent,
+    AdminOrdersComponent,
   ],
   imports: [
     BrowserModule,
