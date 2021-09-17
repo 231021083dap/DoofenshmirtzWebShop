7f8cee8fbbd39a6d8887cdd471cb208f5267018b
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2021-09-17 13:45:36 +0200

    Making charges to StartUp file - added scopes etc etc

--- DoofenshmirtzsWebShop/Startup.cs
+++ DoofenshmirtzsWebShop/Startup.cs
@@ -1,29 +1,25 @@
+using DoofenshmirtzsWebShop.Authorization;
 using DoofenshmirtzsWebShop.Database;
+using DoofenshmirtzsWebShop.Helpers;
 using DoofenshmirtzsWebShop.Repositories;
 using DoofenshmirtzsWebShop.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DoofenshmirtzsWebShop
 {
     public class Startup
     {
-        private readonly IConfiguration _configuration;
+        private readonly string CORSRules = "_CORSRules";
         //private readonly IWebHostEnvironment = _env;
+        private readonly IConfiguration _configuration;
 
-        public Startup(IConfiguration configuration)
+        public Startup( IConfiguration configuration)
         {
             //_env = env;
             _configuration = configuration;
@@ -34,17 +30,50 @@ namespace DoofenshmirtzsWebShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+            options.AddPolicy(name: CORSRules,
+                builder => 
+                    {
+                        builder.WithOrigins("http://localhost.4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    });
+            });
+
+            // Users
+            services.Configure<AppSettings>(_configuration.GetSection("AppSettings"));
+            services.AddScoped<IJwtUtils, JwtUtils>();
+
             services.AddDbContext<DoofenshmirtzWebShopContext>(o => o.UseSqlServer(_configuration.GetConnectionString("Default")));
 
             services.AddScoped<ICategoryService, CategoryServices>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
 
-            
+            services.AddScoped<IOrderService, OrderServices>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.AddScoped<IProductService, ProductServices>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<IUserService, UserServices>();
+            services.AddScoped<IUserRepository, UserRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DoofenshmirtzsWebShop", Version = "v1" });
+                //To enable authorization using Swagger (JWT) 
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JwT",
+                    In = ParameterLocation.Header,
+                    Description = "JwT Authorization header using the Bearer Scheme.  \r\r\r\n Enter 'Bearer'[space] and then your token in the text input below. \r\r\r\n Example: \"Bearer 1234abcdef\"",
+                });
+
             });
         }
 
