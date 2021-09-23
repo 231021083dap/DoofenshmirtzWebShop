6bb69dd0342381f28d44e05c74b5bd30b7065fc2
NepBullV2 <BlackFoenixVEVO@gmail.com>
2021-09-23 09:35:18 +0200

    major admin changes

--- AngularClient/AngularClient/src/app/app-routing.module.ts
+++ AngularClient/AngularClient/src/app/app-routing.module.ts
@@ -1,13 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
-import { AdminDatabaseComponent } from './admin/admin-database/admin-database.component';
-import { AdminNewCategoryComponent } from './admin/admin-new-category/admin-new-category.component';
-import { AdminNewProductComponent } from './admin/admin-new-product/admin-new-product.component';
-import { AdminNewRoleComponent } from './admin/admin-new-role/admin-new-role.component';
-import { AdminNewuserComponent } from './admin/admin-newuser/admin-newuser.component';
-import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { CartComponent } from './cart/cart.component';
 import { ContactComponent } from './contact/contact.component';
 import { FrontPageComponent } from './front-page/front-page.component';
@@ -27,13 +20,6 @@ const routes: Routes = [
   {path: 'signup', component:SignupComponent},
   {path: 'singleproduct', component:SingleProductComponent},
   {path: 'userpage', component:UserPageComponent},
-  {path: 'admin/dashboard', component:AdminDashboardComponent},
-  {path: 'admin/databases', component:AdminDatabaseComponent},
-  {path: 'admin/categories', component:AdminNewCategoryComponent},
-  {path: 'admin/products', component:AdminNewProductComponent},
-  {path: 'admin/roles', component:AdminNewRoleComponent},
-  {path: 'admin/users', component:AdminNewuserComponent},
-  {path: 'admin/orders', component:AdminOrdersComponent}
 ];
 
 @NgModule({
