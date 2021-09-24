1902e8c2de639f1ed0c3a3099935a14adcaa4472
NepBullV2 <BlackFoenixVEVO@gmail.com>
2021-09-24 13:21:43 +0200

    design finished

--- DoofenshmirtzsWebShop/Migrations/20210924092620_REEEEEEEEEE.Designer.cs
+++ DoofenshmirtzsWebShop/Migrations/20210924092620_REEEEEEEEEE.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoofenshmirtzsWebShop.Migrations
 {
     [DbContext(typeof(DoofenshmirtzWebShopContext))]
-    [Migration("20210924080326_nayeon")]
-    partial class nayeon
+    [Migration("20210924092620_REEEEEEEEEE")]
+    partial class REEEEEEEEEE
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
