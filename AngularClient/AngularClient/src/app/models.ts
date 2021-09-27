42dcd99d04bfa555a5a861f4bc3c645841f4c6ed
Jeppe Marcussen <jeppem65@gmail.com>
2021-09-27 12:39:15 +0200

    Added models to model.ts

--- AngularClient/AngularClient/src/app/models.ts
+++ AngularClient/AngularClient/src/app/models.ts
@@ -17,13 +17,28 @@ export interface productGallery{
     galleryPath: string,
     productId?: products
 }
-export interface OrderItems{
+export interface address{
+ID:number,
+customerName:string,
+streetName:string,
+postalCode:number,
+countryName:string
 
+}
+export interface OrderItems{
+ID:number,
+price:number,
+quantity:number,
+orderID:number,
+order?:Orders
+Product:products
 }
 export interface User{
 userID:number,
 email:number,
 username:number
+password:string,
+address:address[]
 }
 export interface Orders{
     orderID:number,
