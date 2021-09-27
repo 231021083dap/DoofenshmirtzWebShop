78cef08e9236b8b23c0e1bbf92c9b74bd2566fdc
Jeppe Marcussen <jeppem65@gmail.com>
2021-09-27 13:02:46 +0200

    OrderService methods

--- AngularClient/AngularClient/src/app/orders.service.ts
+++ AngularClient/AngularClient/src/app/orders.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Orders } from './models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
+  private apiUrl = "https://localhost:5001/api/order";
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'}),
+  };
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getOrders(): Observable<Orders[]>{return this.http.get<Orders[]>(this.apiUrl)};
+  getOrderById(orderID:number): Observable<Orders>{return this.http.get<Orders>(`${this.apiUrl}/${orderID}`)};
+  newOrders(order:Orders): Observable<Orders> {return this.http.post<Orders>(this.apiUrl, order, this.httpOptions)};
 }
