00e93d2ddbcacae2604a704de4e49b2178c72372
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2021-09-28 13:15:33 +0200

    Added Categories to dashboard (Angular)

--- AngularClient/AngularClient/src/app/admin/databases/databases.component.ts
+++ AngularClient/AngularClient/src/app/admin/databases/databases.component.ts
@@ -1,6 +1,7 @@
+import { CategoryService } from 'src/app/category.service';
 import { Component, OnInit } from '@angular/core';
 import { Router, Routes } from '@angular/router';
-import { products } from 'src/app/models';
+import { products, Category } from 'src/app/models';
 import { productService } from 'src/app/product.service';
 
 @Component({
@@ -9,14 +10,24 @@ import { productService } from 'src/app/product.service';
   styleUrls: ['./databases.component.css']
 })
 export class DatabasesComponent implements OnInit {
+  constructor(
+    private productService: productService, private router: Router,
+    private categoryService: CategoryService
+    ) { }
   products:products[] = [];
-  constructor(private productService: productService, private router: Router) { }
+  catories:Category[] = [];
+  
 
   ngOnInit(): void {
   this.productService.getProducts().subscribe(a => this.products = a);
+  this.categoryService.getCategories().subscribe(b => this.catories = b);
   }
 
   isOnProductsTab(): boolean{
     return this.router.url.includes("/admin/databases/products");
+  }
+
+  isOnCategoryTab(): boolean{
+    return this.router.url.includes("/admin/databases/categories");
   }
 }
