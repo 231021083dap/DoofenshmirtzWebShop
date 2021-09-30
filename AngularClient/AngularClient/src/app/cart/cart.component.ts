3b0564fbbc0c5562b5ab086fa406635a49277788
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2021-09-30 13:55:22 +0200

    Login Token given

--- AngularClient/AngularClient/src/app/cart/cart.component.ts
+++ AngularClient/AngularClient/src/app/cart/cart.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  
+
 
   constructor() { }
 
--- DoofenshmirtzsWebShop/DTOs/Responses/LoginResponse.cs
+++ DoofenshmirtzsWebShop/DTOs/Responses/LoginResponse.cs
@@ -11,7 +11,7 @@ namespace DoofenshmirtzsWebShop.DTOs.Responses
         public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+//        public string Password { get; set; }
         public Role Role { get; set; }
         public string Token { get; set; }
     }
