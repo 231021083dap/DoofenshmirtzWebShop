0200d25351fe7e050a6a562af003248428f1103b
Jeppe Marcussen <jeppem65@gmail.com>
2021-09-30 13:28:57 +0200

    Making AdminUserCrud

--- AngularClient/AngularClient/src/app/admin/databases/admin-users/admin-users.component.ts
+++ AngularClient/AngularClient/src/app/admin/databases/admin-users/admin-users.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { emitDistinctChangesOnlyDefaultValue } from '@angular/compiler/src/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Role,
+  User
+} from 'src/app/models';
+import {
+  UserService
+} from 'src/app/user.service';
 
 @Component({
   selector: 'app-admin-users',
@@ -6,10 +17,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-users.component.css']
 })
 export class AdminUsersComponent implements OnInit {
+  users: User[] = [];
+  user: User = {
+    id: 0,
+    email: '',
+    username: '',
+    token: ''
+  }
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.getUsers();
   }
+  getUsers(): void {
+    this.userService.getUsers()
+      .subscribe(a => this.users = a);
+  }
+  cancel(): void {
+    this.user = {
+      id: 0,
+      email: '',
+      username: '',
+      token: ''
+    }
+    
+
+  }
+  edit(user:User):void{
+    this.user = user;
+  }
+  delete(user:User):void{
+    if(confirm('Do you want to delete?')){
+      this.userService.deleteUser(user.id)
+      .subscribe(() => {this.getUsers()})
+    }
+  }
+  save():void{
+    if(this.user.id == 0){
+      this.userService.newUser(this.user)
+      .subscribe(a => {
+        this.users.push(a);
+        this.cancel();
+      })
+    }
+    else{
+      this.userService.updateUser(this.user.id, this.user)
+      .subscribe(() => {this.cancel})
+      this.cancel();
+    }
+  }
+
 
 }
