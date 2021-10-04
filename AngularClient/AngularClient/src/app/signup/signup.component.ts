a2d049f136670172e58f833776bd461136591e72
Jeppe Marcussen <jeppem65@gmail.com>
2021-10-04 11:57:37 +0200

    Sign up register

--- AngularClient/AngularClient/src/app/signup/signup.component.ts
+++ AngularClient/AngularClient/src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  users: User[] = [];
+  user: User = {
+    id: 0,
+    email: '',
+    username: '',
+    password: '',
+    token: ''
+  }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  cancel(): void {
+    this.user = {
+      id: 0,
+      email: '',
+      username: '',
+      password: '',
+    }
+  }
+  save(): void {
+    if (this.user.id == 0) {
+      console.log("Log: Create =", this.user)
+      this.userService.newUser(this.user)
+        .subscribe(a => {
+          this.users.push(a);
+          this.cancel();
+        })
+    }
   }
 
 }
