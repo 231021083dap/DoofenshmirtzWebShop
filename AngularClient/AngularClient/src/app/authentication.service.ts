e91fdde928c66f375dd89c4bff1d30a2d1295e66
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2021-10-05 18:46:20 +0200

    Changes to Authorize on frontend part of the Webshop

--- AngularClient/AngularClient/src/app/authentication.service.ts
+++ AngularClient/AngularClient/src/app/authentication.service.ts
@@ -1,55 +1,49 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { LoginResponseModel } from './models/LoginResponse.model';
+import { Injectable, EventEmitter } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
-@Injectable({ providedIn: 'root' })
+
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthenticationService {
-  private apiUrl = 'https://localhost:5001/api/User';
-  private currentUser;
+  public onAuthenticationChange: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor(private http: HttpClient, private jwt: JwtHelperService) {
-  }
+  constructor(private jwt: JwtHelperService) { 
+    const token = localStorage.getItem('token');
 
-  async user(): Promise<any> {
-    if (this.authenticated()) {
-      let token = localStorage.getItem('token');
-      let dt = this.jwt.decodeToken(token);
-      let id = dt.ID;
-      let headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      });
-      let user = await this.http.get(`${this.apiUrl}/${id}`, {headers}).toPromise();
-
-      return user;
+    if (token) {
+      this.authorization = { token: token }
+      this.onAuthenticationChange.emit(this.authorization);
     }
   }
 
-  authenticated(): boolean {
-    let token = localStorage.getItem('token');
-    if (token) return true;
-    return false;
+  public async login(email: string, password: string): Promise<boolean> {
+    try {
+      let auth = // call to api
+
+      this.authorization = { token: auth.token }
+      localStorage.setItem('token', auth.token);
+      this.onAuthenticationChange.emit(this.authorization);
+
+      return true;
+    } catch(e) {
+      return false;
+    }
   }
 
-  async login(username: string, password: string): Promise<any> {
-    let headers = new HttpHeaders({
-      'Content-Type': 'application/json'
-    });
-
-    let body = {
-      Email: username,
-      Password: password
-    };
-
-    let user = await this.http.post<LoginResponseModel>(`${this.apiUrl}/Authorization`, body, {headers}).toPromise();
-
-    localStorage.setItem('token', user.token);
-  }
-
-  logout() {
+  public logout() {
+    this.authorization = undefined;
     localStorage.removeItem('token');
-    this.currentUser = undefined;
+    this.onAuthenticationChange.emit(this.authorization);
+  }
+
+  get authenticated(): boolean {
+    if (this.authorization) return !this.jwt.isTokenExpired(this.authorization.token);
+    else return false;
+  }
+
+  get id(): number {
+    let data = this.jwt.decodeToken(this.authorization.token);
+    return data.ID;
   }
 }
